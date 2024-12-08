b916bb6bb9df33db2c316655554f7bbb4e6126b4
wildcardjoker <wildcardjoker@users.noreply.github.com>
2024-12-08 14:21:24 +0800

    Refactor to use Monster class instead of tuples
    * Introduced `Monster` class with `Name`, `NumberOfPotions`, and `Symbol` properties.
    * Replaced `monsters` array of tuples with `List<Monster>`.
    * Updated `SumPotionsRequired` method to accept `Monster` object.
    * Modified `SumPotionsRequired` implementation to use `Monster` properties.

--- src/2024/2024-day01/Program.cs
+++ src/2024/2024-day01/Program.cs
@@ -1,12 +1,27 @@
 ﻿var input      = File.ReadAllText("input.txt");
 var characters = input.ToCharArray();
 
-(char monster, int numberOfPotionsPerMonster)[] monsters        = [('A', 0), ('B', 1), ('C', 3)];
-var                                             potionsRequired = monsters.Sum(tuple => SumPotionsRequired(tuple.monster, tuple.numberOfPotionsPerMonster));
+List<Monster> monsters =
+[
+    new ("A", 0),
+    new ("B", 1),
+    new ("C", 3)
+];
+
+var potionsRequired = monsters.Sum(SumPotionsRequired);
 Console.WriteLine($"Part 1: {potionsRequired} potions required.");
 return;
 
-int SumPotionsRequired(char monster, int numberOfPotionsPerMonster)
+int SumPotionsRequired(Monster monster)
 {
-    return characters.Count(c => c.Equals(monster)) * numberOfPotionsPerMonster;
+    return characters.Count(c => c.Equals(monster.Symbol)) * monster.NumberOfPotions;
+}
+
+internal class Monster(string name, int numberOfPotions)
+{
+    #region Properties
+    public string Name            {get;} = name;
+    public int    NumberOfPotions {get;} = numberOfPotions;
+    public char   Symbol          {get;} = name[0];
+    #endregion
 }
