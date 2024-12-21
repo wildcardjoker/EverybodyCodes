3f4de31d98bb8e190f81d1d1b62e787d90ccf658
wildcardjoker <wildcardjoker@users.noreply.github.com>
2024-12-21 15:17:22 +0800

    Completed 2024 Q05 Part 1.
    *Refactor methods and remove unused directives
    * Removed `#region Using Directives` and `#endregion` from `Program.cs`.
    * Modified `SolvePart1` to call `SolvePart2` and `SolvePart3` and added a `return` statement.
    * Refactored `columns` list initialization into `InitializeColumns` method.
    * Refactored `columns` list population into `PopulateColumns` method.
    * Refactored column processing loop into `ProcessColumns` method.
    * Refactored result display into `DisplayResults` method.
    * Removed `DisplayColumns` method.
    * Added `SolvePart3` method to read from `input3.txt` and print a placeholder result.

--- src/2024/2024-05/Program.cs
+++ src/2024/2024-05/Program.cs
@@ -1,7 +1,4 @@
-﻿#region Using Directives
-#endregion
-
-SolvePart1();
+﻿SolvePart1();
 SolvePart2();
 SolvePart3();
 return;
@@ -9,60 +6,84 @@ return;
 void SolvePart1()
 {
     var lines   = File.ReadAllLines("input1.txt").Select(s => s.Replace(" ", "")).ToList();
-    var columns = new List<List<int>>();
+    var columns = InitializeColumns(lines[0].Length);
 
-    for (var i = 0; i < lines[0].Length; i++)
-    {
-        columns.Add([]);
-    }
-
-    foreach (var line in lines)
-    {
-        for (var i = 0; i < line.Length; i++)
-        {
-            columns[i].Add(int.Parse(line[i].ToString()));
-        }
-    }
+    PopulateColumns(lines, columns);
 
     const int numRounds = 10;
     var       results   = new List<string>();
     while (results.Count < numRounds)
     {
-        for (var column = 0; column < columns.Count; column++)
-        {
-            var nextColumn = (column + 1) % columns.Count;
-            var clapper    = columns[column][0];
-            columns[column].RemoveAt(0);
-            var absorbed = false;
-            var count    = 0;
-            var index    = 0;
-            do
-            {
-                if (index == columns[nextColumn].Count)
-                {
-                    index--;
-                }
-
-                count++;
-
-                if (count == clapper)
-                {
-                    columns[nextColumn].Insert(index, clapper);
-                    absorbed = true;
-
-                    break;
-                }
-
-                index++;
-            }
-            while (!absorbed);
-
-            var number = string.Join(string.Empty, columns.Select(col => col[0]));
-            DisplayColumns(columns);
-            results.Add(number);
-        }
+        ProcessColumns(columns, results);
     }
 
+    DisplayResults(numRounds, results);
+}
+
+List<List<int>> InitializeColumns(int length)
+{
+    // Create a column for each character
+    var columns = new List<List<int>>();
+    for (var i = 0; i < length; i++)
+    {
+        columns.Add([]);
+    }
+
+    return columns;
+}
+
+void PopulateColumns(List<string> lines, List<List<int>> columns)
+{
+    // Read each line
+    foreach (var line in lines)
+    {
+        // Read each character in the line
+        for (var i = 0; i < line.Length; i++)
+        {
+            // Add the character to the appropriate column
+            columns[i].Add(int.Parse(line[i].ToString()));
+        }
+    }
+}
+
+void ProcessColumns(List<List<int>> columns, List<string> results)
+{
+    for (var column = 0; column < columns.Count; column++)
+    {
+        var nextColumn = (column + 1) % columns.Count;
+        var clapper    = columns[column][0];
+        columns[column].RemoveAt(0);
+        var absorbed = false;
+        var count    = 0;
+        var index    = 0;
+        do
+        {
+            if (index == columns[nextColumn].Count)
+            {
+                index--;
+            }
+
+            count++;
+
+            if (count == clapper)
+            {
+                columns[nextColumn].Insert(index, clapper);
+                absorbed = true;
+
+                break;
+            }
+
+            index++;
+        }
+        while (!absorbed);
+
+        var number = string.Join(string.Empty, columns.Select(col => col[0]));
+        results.Add(number);
+    }
+}
+
+void DisplayResults(int numRounds, List<string> results)
+{
     for (var i = 0; i < numRounds; i++)
     {
         Console.WriteLine($"{i + 1}: {results[i]}");
@@ -81,14 +102,4 @@ void SolvePart3()
     var input  = File.ReadAllText("input3.txt");
     var result = 0;
     Console.WriteLine($"Part 3 result: {result}");
-}
-
-void DisplayColumns(List<List<int>> columns)
-{
-    foreach (var column in columns)
-    {
-        Console.WriteLine(string.Join(" ", column));
-    }
-
-    Console.WriteLine();
 }
